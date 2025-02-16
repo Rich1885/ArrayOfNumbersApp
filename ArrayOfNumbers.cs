0c7326ef497fac52911fc92cb504aa842e805201
Rich1885 <mecsekirichard@gmail.com>
2025-02-16 16:50:50 +0000

    Initial commit

---
+++ ArrayOfNumbers.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayOfNumbersApp
+{
+    internal class ArrayOfNumbers
+    {
+        private int[] numbers;
+
+        public ArrayOfNumbers(int size)
+        {
+            if(size <= 0)
+            {
+                throw new ArgumentOutOfRangeException("array size must be greater than zero");
+            }
+
+            numbers = new int[size];
+        } 
+
+
+        public ArrayOfNumbers(int[] inputArray)
+        {
+            if(inputArray == null)
+            {
+                throw new ArgumentNullException("array cannot be null");
+            }
+
+            numbers = new int[inputArray.Length];
+
+            for(int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = inputArray[i];
+            }
+
+        }
+    }
+}
